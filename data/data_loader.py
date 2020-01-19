f1bc3605e0d9028047511d8461ead6de9a86a5cb
Howrunz <zhrwwd9695@sina.com>
2020-01-19 21:13:48 +0800

    data process

---
+++ data/data_loader.py
@@ -0,0 +1,21 @@
+import glob
+from os.path import splitext
+from os import listdir
+from torch.utils.data import Dataset
+from PIL import Image
+import torch
+
+class DatasetTemplate(Dataset):
+    def __init__(self, img_dir, mask_dir):
+        self.imgs_dir = img_dir
+        self.masks_dir = mask_dir
+        self.f_list = [splitext(file)[0] for file in listdir(self.imgs_dir)]
+
+    def __getitem__(self, item):
+        index = self.f_list[item]
+        image = Image.open(glob.glob(self.imgs_dir + index + '*'))
+        mask = Image.open(glob.glob(self.masks_dir + index + '*')) # the name of mask may different from the image
+        return torch.from_numpy(image), torch.from_numpy(mask)
+
+    def __len__(self):
+        return len(self.f_list)
