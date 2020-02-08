38b5f35e642e3b3d5ef30ea34b7516e3f3f00901
Howrunz <zhrwwd9695@sina.com>
2020-02-08 23:43:09 +0800

    add module

---
+++ module/convolution.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+class conv_block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(conv_block, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        result = self.conv(x)
+        return result
+
+class up_sample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(up_sample, self).__init__()
+
+        self.up_sample = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        result = self.up_sample(x)
+        return result
