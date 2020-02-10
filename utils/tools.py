1e34860fbd15403ac350ad54b10fedbd75e5a2e5
Howrunz <zhrwwd9695@sina.com>
2020-02-10 22:10:01 +0800

    add save weight

--- utils/tools.py
+++ utils/tools.py
@@ -5,3 +5,13 @@ def print_model_summary(model):
     for name, parameter in model.named_paraneters():
         print(name, parameter.numel(), parameter.requires_grad)
     return None
+
+def save_weight(model, model_path, train_loss, valid_loss, epoch, step):
+    torch.save(
+        {
+            'model': model.state_dict(),
+            'epoch': epoch,
+            'step': step,
+            'train_loss': train_loss,
+            'valid_loss': valid_loss
+        }, str(model_path))
