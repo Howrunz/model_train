d1f4c489659a8ed194b24d80c67ff7ddb6277492
Howrunz <zhrwwd9695@sina.com>
2020-02-14 18:52:27 +0800

    load data

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -1,66 +1,51 @@
-import glob
 import pandas as pd
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from os.path import splitext
-from os import listdir
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image
 import torch
 
 class DatasetTemplate(Dataset):
-    def __init__(self, img_dir, mask_dir, csv_file):
+    def __init__(self, cfg, img_dir, mask_dir, csv_dir, status='train'):
+        self.config = cfg
         self.imgs_dir = img_dir
         self.masks_dir = mask_dir
+        self.status = status
+        assert self.status in ['train', 'validation'], 'Please input status as train or validation'
+        height = cfg['DATASET']['Height']
+        width = cfg['DATASET']['Width']
+        channel = cfg['DATASET']['Channel']
 
-        ##### temp now, to yaml ###
-        height = 256
-        width = 256
-        channel = 3
-        ###### ###### ###### ######
-
-        data = pd.read_csv(csv_file, dtype=str)
-        train_num = data['Type'][data.Type == 'train'].value_counts().values[0]
-        val_num = data['Type'][data.Type == 'validation'].value_counts().values[0]
-        # test_num = data['Type'][data.Type == 'test'].value_counts().values[0]
-        train_image = np.empty((train_num, height, width, channel))
-        train_mask = np.empty((train_num, height, width))
-        valid_image = np.empty((val_num, height, width, channel))
-        valid_mask = np.empty((val_num, height, width))
-        # test_image = np.empty((test_num, height, width, channel))
-        # test_mask = np.empty((test_num, height, width))
-        for num in range(train_num):
+        self.csv_file = csv_dir + self.status + '_list.csv'
+        data = pd.read_csv(self.csv_file, dtype=str)
+        self.data_num = data['Type'][data.Type == self.status].value_counts().values[0]
+        image_set = np.empty((self.data_num, height, width, channel))
+        mask_set = np.empty((self.data_num, height, width))
+        for num in range(self.data_num):
             image_path = self.imgs_dir + data['ID'][num] + '.jpg' # the name of image may different from the mask
             mask_path = self.masks_dir + data['ID'][num] + '_mask.jpg' # the name of mask may different from the image
             image = load_img(image_path, target_size=(height, width), color_mode='rgb') # 'target_size' can resize image
             mask = load_img(mask_path, target_size=(height, width), color_mode='grayscale')
             image_arr = img_to_array(image)
             mask_arr = img_to_array(mask)
-            train_image[num] = image_arr
-            train_mask[num] = mask_arr
-        for num in range(train_num, train_num+val_num):
-            image_path = self.imgs_dir + data['ID'][num] + '.jpg'
-            mask_path = self.masks_dir + data['ID'][num] + '_mask.jpg'
-            image = load_img(image_path, target_size=(height, width), color_mode='rgb')
-            mask = load_img(mask_path, target_size=(height, width), color_mode='grayscale')
-            image_arr = img_to_array(image)
-            mask_arr = img_to_array(mask)
-            valid_image[num] = image_arr
-            valid_mask[num] = mask_arr
-        self.train_image = np.transpose(train_image, (0,3,1,2))
-        self.valid_image = np.transpose(valid_image, (0,3,1,2))
-        self.train_mask = np.resize(train_mask, (train_num, 1, height, width))
-        self.valid_mask = np.resize(valid_mask, (val_num, 1, height, width))
+            image_set[num] = image_arr
+            mask_set[num] = mask_arr
+        self.image = np.transpose(image_set, (0,3,1,2))
+        self.mask = np.resize(mask_set, (self.data_num, 1, height, width))
 
     def __getitem__(self, item):
-        image = self.train_image[item]
-        mask = self.train_mask[item]
+        image = self.image[item]
+        mask = self.mask[item]
         return torch.from_numpy(image), torch.from_numpy(mask)
 
     def __len__(self):
-        return len(self.f_list)
+        return self.data_num
 
-def get_loader(img_dir, mask_dir, args, shuffle=True):
-    dataset = DatasetTemplate(img_dir, mask_dir)
-    data_loader = DataLoader(dataset, args.batch_size, shuffle, num_workers=args.workers)
+def get_loader(cfg, img_dir, mask_dir, csv_dir, shuffle=True, status='train'):
+    dataset = DatasetTemplate(cfg, img_dir, mask_dir, csv_dir, status=status)
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=cfg['MODEL']['Batch_size'],
+        shuffle=shuffle,
+        num_workers=cfg['MODEL']['Num_workers']
+    )
     return data_loader
