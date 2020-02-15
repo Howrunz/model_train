2de704ffbddc8088658138ae04c2cc5a53a33143
Howrunz <zhrwwd9695@sina.com>
2020-02-15 21:49:47 +0800

    load data

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -4,7 +4,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_im
 from torch.utils.data import Dataset, DataLoader
 import torch
 
-class DatasetTemplate(Dataset):
+class TrainDataset(Dataset):
     def __init__(self, cfg, img_dir, mask_dir, csv_dir, status='train'):
         self.config = cfg
         self.imgs_dir = img_dir
@@ -40,8 +40,42 @@ class DatasetTemplate(Dataset):
     def __len__(self):
         return self.data_num
 
+class TestDataset(Dataset):
+    def __init__(self, cfg, image_dir, csv_file):
+        self.image_dir = image_dir
+        self.csv = csv_file
+        height = cfg['DATASET']['Height']
+        width = cfg['DATASET']['Width']
+        channel = cfg['DATASET']['Channel']
+        self.data = pd.read_csv(self.csv, dtype=str)
+        self.data_num = self.data['Type'][self.data.Type == 'test'].value_counts().values[0]
+        image_set = np.empty((self.data_num, height, width, channel))
+        for num in range(self.data_num):
+            image_path = self.image_dir + self.data['ID'][num] + '.jpg'
+            image = load_img(image_path, target_size=(height, width), color_mode='rgb')
+            image_arr = img_to_array(image)
+            image_set[num] = image_arr
+        self.image = np.transpose(image_set, (0,3,1,2))
+
+    def __getitem__(self, item):
+        image = self.image[item]
+        return image, self.data['ID'][item]
+
+    def __len__(self):
+        return self.data_num
+
 def get_loader(cfg, img_dir, mask_dir, csv_dir, shuffle=True, status='train'):
-    dataset = DatasetTemplate(cfg, img_dir, mask_dir, csv_dir, status=status)
+    dataset = TrainDataset(cfg, img_dir, mask_dir, csv_dir, status=status)
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=cfg['MODEL']['Batch_size'],
+        shuffle=shuffle,
+        num_workers=cfg['MODEL']['Num_workers']
+    )
+    return data_loader
+
+def get_test_loader(cfg, img_dir, csv_file, shuffle=False):
+    dataset = TestDataset(cfg, img_dir, csv_file)
     data_loader = DataLoader(
         dataset=dataset,
         batch_size=cfg['MODEL']['Batch_size'],
