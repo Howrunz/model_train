a7c07bf766fb57c3f916e7bffffa395c4588445b
Howrunz <zhrwwd9695@sina.com>
2020-02-16 19:06:54 +0800

    validation

--- validation.py
+++ validation.py
@@ -5,6 +5,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 def validation(model:nn.Module, criterion, valid_loader, device):
+    val_loss = 0.
+    validation_pred = []
+    validation_true = []
     with torch.no_grad():
         start_time = time.time()
         for image, mask in valid_loader:
@@ -12,6 +15,13 @@ def validation(model:nn.Module, criterion, valid_loader, device):
             mask = mask.to(device)
             output = model(image)
             loss = criterion(output, mask)
+            val_loss += loss.item()
+            output_np = output.detach().cpu().numpy()
+            mask_np = mask.detach().cpu().numpy()
+            validation_pred.extend([output_np[s] for s in range(output_np.shape[0])])
+            validation_true.extend([mask_np[s] for s in range(mask_np.shape[0])])
         end_time = time.time() - start_time
         print('vaildation epoch time: ', end_time)
-    return loss
+        # TODO
+        # calcuate criticize
+    return val_loss #, criticize
