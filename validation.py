f3a609765eaf83f80398ae0ede0dd6b3e55b80d2
Howrunz <zhrwwd9695@sina.com>
2020-02-17 21:39:15 +0800

    validation

--- validation.py
+++ validation.py
@@ -1,4 +1,5 @@
 import time
+from utils.evaluation import evaluate_dice
 
 import torch
 import torch.nn as nn
@@ -22,6 +23,6 @@ def validation(model:nn.Module, criterion, valid_loader, device):
             validation_true.extend([mask_np[s] for s in range(mask_np.shape[0])])
         end_time = time.time() - start_time
         print('vaildation epoch time: ', end_time)
-        # TODO
-        # calcuate criticize
-    return val_loss #, criticize
+    val_dice = evaluate_dice(validation_pred, validation_true)
+    print('validation dice: %4f' % val_dice)
+    return val_loss
