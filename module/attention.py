88ce0de90f65f3a8b023abd81898acc72ad0ee25
Howrunz <zhrwwd9695@sina.com>
2020-02-22 22:28:22 +0800

    attention module

--- module/attention.py
+++ module/attention.py
@@ -1,13 +1,12 @@
-from collections import OrderedDict
-
 import torch
 import torch.nn as nn
-import tensorboardX
-from tensorboardX import SummaryWriter
-from torch.autograd import Variable
+from torch.nn import init
+from torch.nn import functional as F
 
 import warnings
 warnings.simplefilter("ignore")
+
+'''code from https://github.com/ozan-oktay/Attention-Gated-Networks'''
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -56,6 +55,7 @@ def weights_init_orthogonal(m):
         init.normal(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
 
+
 def init_weights(net, init_type='normal'):
     #print('initialization method [%s]' % init_type)
     if init_type == 'normal':
@@ -70,21 +70,22 @@ def init_weights(net, init_type='normal'):
         raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
 
 class _GridAttentionBlockND(nn.Module):
-    def __init__(self, in_channels, gating_channels, inter_channels=None, dimension=2, mode='concatenation'):#sub_sample_factor=(2,2,2)):
+    def __init__(self, in_channels, gating_channels, inter_channels=None, dimension=3, mode='concatenation',
+                 sub_sample_factor=(2,2,2)):
         super(_GridAttentionBlockND, self).__init__()
 
         assert dimension in [2, 3]
         assert mode in ['concatenation', 'concatenation_debug', 'concatenation_residual']
 
         # Downsampling rate for the input featuremap
-        #if isinstance(sub_sample_factor, tuple): self.sub_sample_factor = sub_sample_factor
-        #elif isinstance(sub_sample_factor, list): self.sub_sample_factor = tuple(sub_sample_factor)
-        #else: self.sub_sample_factor = tuple([sub_sample_factor]) * dimension
+        if isinstance(sub_sample_factor, tuple): self.sub_sample_factor = sub_sample_factor
+        elif isinstance(sub_sample_factor, list): self.sub_sample_factor = tuple(sub_sample_factor)
+        else: self.sub_sample_factor = tuple([sub_sample_factor]) * dimension
 
         # Default parameter set
         self.mode = mode
         self.dimension = dimension
-        #self.sub_sample_kernel_size = self.sub_sample_factor
+        self.sub_sample_kernel_size = self.sub_sample_factor
 
         # Number of channels (pixel dimensions)
         self.in_channels = in_channels
@@ -115,7 +116,7 @@ class _GridAttentionBlockND(nn.Module):
 
         # Theta^T * x_ij + Phi^T * gating_signal + bias
         self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
-                             kernel_size=2, stride=2, padding=0, bias=False)
+                             kernel_size=self.sub_sample_kernel_size, stride=self.sub_sample_factor, padding=0, bias=False)
         self.phi = conv_nd(in_channels=self.gating_channels, out_channels=self.inter_channels,
                            kernel_size=1, stride=1, padding=0, bias=True)
         self.psi = conv_nd(in_channels=self.inter_channels, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
@@ -146,7 +147,6 @@ class _GridAttentionBlockND(nn.Module):
         return output
 
     def _concatenation(self, x, g):
-
         input_size = x.size()
         batch_size = input_size[0]
         assert batch_size == g.size(0)
@@ -223,66 +223,243 @@ class _GridAttentionBlockND(nn.Module):
 
         return W_y, sigm_psi_f
 
+
 class GridAttentionBlock2D(_GridAttentionBlockND):
-    def __init__(self, in_channels, gating_channels, inter_channels=None, mode='concatenation'):
-                 #sub_sample_factor=(2,2,2)):
+    def __init__(self, in_channels, gating_channels, inter_channels=None, mode='concatenation',
+                 sub_sample_factor=(2,2,2)):
         super(GridAttentionBlock2D, self).__init__(in_channels,
                                                    inter_channels=inter_channels,
                                                    gating_channels=gating_channels,
-                                                   dimension=2, mode=mode)
-                                                   #sub_sample_factor=sub_sample_factor,
-                                                   #)
-
-class MultiAttentionBlock(nn.Module):
-    def __init__(self, in_size, gate_size, inter_size, nonlocal_mode):#, sub_sample_factor):
-        super(MultiAttentionBlock, self).__init__()
-        self.gate_block_1 = GridAttentionBlock2D(in_channels=in_size, gating_channels=gate_size,
-                                                 inter_channels=inter_size, mode=nonlocal_mode)
-                                                 #sub_sample_factor= sub_sample_factor)
-        self.combine_gates = nn.Sequential(nn.Conv2d(in_size, in_size, kernel_size=1, stride=1, padding=0),
-                                           nn.BatchNorm2d(in_size),
-                                           nn.ReLU(inplace=True)
-                                           )
-
-        # initialise the blocks
-        for m in self.children():
-            if m.__class__.__name__.find('GridAttentionBlock2D') != -1: continue
-            init_weights(m, init_type='kaiming')
-
-    def forward(self, input, gating_signal):
-        gate_1, attention_1 = self.gate_block_1(input, gating_signal)
-
-        return self.combine_gates(gate_1), attention_1
+                                                   dimension=2, mode=mode,
+                                                   sub_sample_factor=sub_sample_factor,
+                                                   )
 
 
-class UnetGridGatingSignal(nn.Module):
-    def __init__(self, in_size, out_size, kernel_size=1, is_batchnorm=True):
-        super(UnetGridGatingSignal, self).__init__()
+class GridAttentionBlock3D(_GridAttentionBlockND):
+    def __init__(self, in_channels, gating_channels, inter_channels=None, mode='concatenation',
+                 sub_sample_factor=(2,2,2)):
+        super(GridAttentionBlock3D, self).__init__(in_channels,
+                                                   inter_channels=inter_channels,
+                                                   gating_channels=gating_channels,
+                                                   dimension=3, mode=mode,
+                                                   sub_sample_factor=sub_sample_factor,
+                                                   )
 
-        if is_batchnorm:
-            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size, stride=1, padding=0),
-                                       nn.BatchNorm2d(out_size),
-                                       nn.ReLU(inplace=True),
-                                       )
+class _GridAttentionBlockND_TORR(nn.Module):
+    def __init__(self, in_channels, gating_channels, inter_channels=None, dimension=3, mode='concatenation',
+                 sub_sample_factor=(1,1,1), bn_layer=True, use_W=True, use_phi=True, use_theta=True, use_psi=True, nonlinearity1='relu'):
+        super(_GridAttentionBlockND_TORR, self).__init__()
+
+        assert dimension in [2, 3]
+        assert mode in ['concatenation', 'concatenation_softmax',
+                        'concatenation_sigmoid', 'concatenation_mean',
+                        'concatenation_range_normalise', 'concatenation_mean_flow']
+
+        # Default parameter set
+        self.mode = mode
+        self.dimension = dimension
+        self.sub_sample_factor = sub_sample_factor if isinstance(sub_sample_factor, tuple) else tuple([sub_sample_factor])*dimension
+        self.sub_sample_kernel_size = self.sub_sample_factor
+
+        # Number of channels (pixel dimensions)
+        self.in_channels = in_channels
+        self.gating_channels = gating_channels
+        self.inter_channels = inter_channels
+
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+
+        if dimension == 3:
+            conv_nd = nn.Conv3d
+            bn = nn.BatchNorm3d
+            self.upsample_mode = 'trilinear'
+        elif dimension == 2:
+            conv_nd = nn.Conv2d
+            bn = nn.BatchNorm2d
+            self.upsample_mode = 'bilinear'
         else:
-            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, kernel_size, stride=1, padding=0),
-                                       nn.ReLU(inplace=True),
-                                       )
+            raise NotImplemented
 
-        # initialise the blocks
+        # initialise id functions
+        # Theta^T * x_ij + Phi^T * gating_signal + bias
+        self.W = lambda x: x
+        self.theta = lambda x: x
+        self.psi = lambda x: x
+        self.phi = lambda x: x
+        self.nl1 = lambda x: x
+
+        if use_W:
+            if bn_layer:
+                self.W = nn.Sequential(
+                    conv_nd(in_channels=self.in_channels, out_channels=self.in_channels, kernel_size=1, stride=1, padding=0),
+                    bn(self.in_channels),
+                )
+            else:
+                self.W = conv_nd(in_channels=self.in_channels, out_channels=self.in_channels, kernel_size=1, stride=1, padding=0)
+
+        if use_theta:
+            self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                                 kernel_size=self.sub_sample_kernel_size, stride=self.sub_sample_factor, padding=0, bias=False)
+
+
+        if use_phi:
+            self.phi = conv_nd(in_channels=self.gating_channels, out_channels=self.inter_channels,
+                               kernel_size=self.sub_sample_kernel_size, stride=self.sub_sample_factor, padding=0, bias=False)
+
+
+        if use_psi:
+            self.psi = conv_nd(in_channels=self.inter_channels, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
+
+
+        if nonlinearity1:
+            if nonlinearity1 == 'relu':
+                self.nl1 = lambda x: F.relu(x, inplace=True)
+
+        if 'concatenation' in mode:
+            self.operation_function = self._concatenation
+        else:
+            raise NotImplementedError('Unknown operation function.')
+
+        # Initialise weights
         for m in self.children():
             init_weights(m, init_type='kaiming')
 
-    def forward(self, inputs):
-        outputs = self.conv1(inputs)
-        return outputs
+
+        if use_psi and self.mode == 'concatenation_sigmoid':
+            nn.init.constant(self.psi.bias.data, 3.0)
+
+        if use_psi and self.mode == 'concatenation_softmax':
+            nn.init.constant(self.psi.bias.data, 10.0)
+
+        # if use_psi and self.mode == 'concatenation_mean':
+        #     nn.init.constant(self.psi.bias.data, 3.0)
+
+        # if use_psi and self.mode == 'concatenation_range_normalise':
+        #     nn.init.constant(self.psi.bias.data, 3.0)
+
+        parallel = False
+        if parallel:
+            if use_W: self.W = nn.DataParallel(self.W)
+            if use_phi: self.phi = nn.DataParallel(self.phi)
+            if use_psi: self.psi = nn.DataParallel(self.psi)
+            if use_theta: self.theta = nn.DataParallel(self.theta)
+
+    def forward(self, x, g):
+        '''
+        :param x: (b, c, t, h, w)
+        :param g: (b, g_d)
+        :return:
+        '''
+
+        output = self.operation_function(x, g)
+        return output
+
+    def _concatenation(self, x, g):
+        input_size = x.size()
+        batch_size = input_size[0]
+        assert batch_size == g.size(0)
+
+        #############################
+        # compute compatibility score
+
+        # theta => (b, c, t, h, w) -> (b, i_c, t, h, w)
+        # phi   => (b, c, t, h, w) -> (b, i_c, t, h, w)
+        theta_x = self.theta(x)
+        theta_x_size = theta_x.size()
+
+        #  nl(theta.x + phi.g + bias) -> f = (b, i_c, t/s1, h/s2, w/s3)
+        phi_g = F.upsample(self.phi(g), size=theta_x_size[2:], mode=self.upsample_mode)
+
+        f = theta_x + phi_g
+        f = self.nl1(f)
+
+        psi_f = self.psi(f)
+
+        ############################################
+        # normalisation -- scale compatibility score
+        #  psi^T . f -> (b, 1, t/s1, h/s2, w/s3)
+        if self.mode == 'concatenation_softmax':
+            sigm_psi_f = F.softmax(psi_f.view(batch_size, 1, -1), dim=2)
+            sigm_psi_f = sigm_psi_f.view(batch_size, 1, *theta_x_size[2:])
+        elif self.mode == 'concatenation_mean':
+            psi_f_flat = psi_f.view(batch_size, 1, -1)
+            psi_f_sum = torch.sum(psi_f_flat, dim=2)#clamp(1e-6)
+            psi_f_sum = psi_f_sum[:,:,None].expand_as(psi_f_flat)
+
+            sigm_psi_f = psi_f_flat / psi_f_sum
+            sigm_psi_f = sigm_psi_f.view(batch_size, 1, *theta_x_size[2:])
+        elif self.mode == 'concatenation_mean_flow':
+            psi_f_flat = psi_f.view(batch_size, 1, -1)
+            ss = psi_f_flat.shape
+            psi_f_min = psi_f_flat.min(dim=2)[0].view(ss[0],ss[1],1)
+            psi_f_flat = psi_f_flat - psi_f_min
+            psi_f_sum = torch.sum(psi_f_flat, dim=2).view(ss[0],ss[1],1).expand_as(psi_f_flat)
+
+            sigm_psi_f = psi_f_flat / psi_f_sum
+            sigm_psi_f = sigm_psi_f.view(batch_size, 1, *theta_x_size[2:])
+        elif self.mode == 'concatenation_range_normalise':
+            psi_f_flat = psi_f.view(batch_size, 1, -1)
+            ss = psi_f_flat.shape
+            psi_f_max = torch.max(psi_f_flat, dim=2)[0].view(ss[0], ss[1], 1)
+            psi_f_min = torch.min(psi_f_flat, dim=2)[0].view(ss[0], ss[1], 1)
+
+            sigm_psi_f = (psi_f_flat - psi_f_min) / (psi_f_max - psi_f_min).expand_as(psi_f_flat)
+            sigm_psi_f = sigm_psi_f.view(batch_size, 1, *theta_x_size[2:])
+
+        elif self.mode == 'concatenation_sigmoid':
+            sigm_psi_f = F.sigmoid(psi_f)
+        else:
+            raise NotImplementedError
+
+        # sigm_psi_f is attention map! upsample the attentions and multiply
+        sigm_psi_f = F.upsample(sigm_psi_f, size=input_size[2:], mode=self.upsample_mode)
+        y = sigm_psi_f.expand_as(x) * x
+        W_y = self.W(y)
+
+        return W_y, sigm_psi_f
 
 
-class UnetDsv2(nn.Module):
-    def __init__(self, in_channels, out_channels, scale_factor):
-        super(UnetDsv2, self).__init__()
-        self.dsv = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0),
-                                 nn.Upsample(scale_factor=scale_factor, mode='bilinear'), )
+class GridAttentionBlock2D_TORR(_GridAttentionBlockND_TORR):
+    def __init__(self, in_channels, gating_channels, inter_channels=None, mode='concatenation',
+                 sub_sample_factor=(1,1), bn_layer=True,
+                 use_W=True, use_phi=True, use_theta=True, use_psi=True,
+                 nonlinearity1='relu'):
+        super(GridAttentionBlock2D_TORR, self).__init__(in_channels,
+                                               inter_channels=inter_channels,
+                                               gating_channels=gating_channels,
+                                               dimension=2, mode=mode,
+                                               sub_sample_factor=sub_sample_factor,
+                                               bn_layer=bn_layer,
+                                               use_W=use_W,
+                                               use_phi=use_phi,
+                                               use_theta=use_theta,
+                                               use_psi=use_psi,
+                                               nonlinearity1=nonlinearity1)
 
-    def forward(self, input):
-        return self.dsv(input)
+
+class GridAttentionBlock3D_TORR(_GridAttentionBlockND_TORR):
+    def __init__(self, in_channels, gating_channels, inter_channels=None, mode='concatenation',
+                 sub_sample_factor=(1,1,1), bn_layer=True):
+        super(GridAttentionBlock3D_TORR, self).__init__(in_channels,
+                                                   inter_channels=inter_channels,
+                                                   gating_channels=gating_channels,
+                                                   dimension=3, mode=mode,
+                                                   sub_sample_factor=sub_sample_factor,
+                                                   bn_layer=bn_layer)
+
+
+
+if __name__ == '__main__':
+    from torch.autograd import Variable
+
+    mode_list = ['concatenation']
+
+    for mode in mode_list:
+
+        img = Variable(torch.rand(2, 16, 10, 10, 10))
+        gat = Variable(torch.rand(2, 64, 4, 4, 4))
+        net = GridAttentionBlock3D(in_channels=16, inter_channels=16, gating_channels=64, mode=mode, sub_sample_factor=(2,2,2))
+        out, sigma = net(img, gat)
+        print(out.size())
