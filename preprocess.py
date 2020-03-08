f492e5c83063b0993de66a06417b954161449931
Howrunz <zhrwwd9695@sina.com>
2020-03-08 22:05:18 +0800

    prepare data

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,14 @@
-from utils import tools
+from utils import config
 import os
 import random
 import pandas as pd
+import argparse
 
-def main():
-    cfg = tools.read_yaml('./config/train_config.yaml')
-    file_name = os.listdir(cfg['DIRECTORY']['image_dir'])
-    random.seed(42)
+def main(args):
+    opj = os.path.join
+    cfg = config.Config(args.conf)
+    file_name = os.listdir(cfg.image_dir)
+    random.seed(cfg.Random_seed)
     files = []
     for file in file_name:
         files.append(file[0:12])  # different dataset use different range
@@ -20,10 +22,19 @@ def main():
     train_df = pd.DataFrame({'ID': files[0:train_data_num], 'Type': 'train'})
     valid_df = pd.DataFrame({'ID': files[train_data_num:(train_data_num+valid_data_num)], 'Type': 'valid'})
     test_df = pd.DataFrame({'ID': files[(train_data_num+valid_data_num):all_data_num], 'Type': 'valid'})
-    train_df.to_csv('./data/train_list.csv', index=False)
-    valid_df.to_csv('./data/valid_list.csv', index=False)
-    test_df.to_csv('./data/test_list.csv', index=False)
-    print('Create CSV file successfully')
+    train_csv_path = opj(cfg.csv_dir, args.csvn + '_train.csv')
+    valid_csv_path = opj(cfg.csv_dir, args.csvn + '_valid.csv')
+    test_csv_path = opj(cfg.csv_dir, args.csvn + '_test.csv')
+    train_df.to_csv(train_csv_path, index=False)
+    valid_df.to_csv(valid_csv_path, index=False)
+    test_df.to_csv(test_csv_path, index=False)
+    if os.path.exists(train_csv_path) and os.path.exists(valid_csv_path) and os.path.exists(test_csv_path):
+        print('Create CSV file successfully!')
 
 if __name__ == '__main__':
-    main()
+    parse = argparse.ArgumentParser()
+    arg = parse.add_argument
+    arg('--conf', type=str, default='./config/train_config.yaml', help='config file path')
+    arg('--csvn', type=str, default='dataset', help='csv file name')
+    args = parse.parse_args()
+    main(args)
